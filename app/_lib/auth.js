4fb7324d033a5bfc9b05b348854e1268cda29255
Subish GC <subishgc2022@gmail.com>
2025-03-08 13:53:10 +0545

    added a protected route

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -9,6 +9,14 @@ const authConfig = {
       clientSecret: process.env.AUTH_GOOGLE_SECRET,
     }),
   ],
+  // authorized determines whether a user is authorized to access a specific page or resource in your application.
+  callbacks: {
+    authorized({ auth, request }) {
+      // auth gives the info about current session like user id , name , email etc
+      return !!auth?.user;
+      //double exclamation here is the trick to convert any value into boolean (true or false.)
+    },
+  },
   secret: process.env.NEXTAUTH_SECRET, // Added for security
 };
 
@@ -16,3 +24,11 @@ export const {
   auth,
   handlers: { GET, POST },
 } = NextAuth(authConfig);
+
+//https://grok.com/chat/0c57aaf7-0724-447e-ae26-e7c222d88167
+
+// When does the authorized function get called?
+// The authorized callback (optional in the callbacks object) is invoked by the auth middleware when a visitor attempts to access a protected route (e.g., /account, specified in matcher).
+// It determines if access is granted, such as by checking if the visitor is authenticated.
+//if the user is unauthenticated then by default the user is redirected to the signin page.
+//check it yourself...
--- middleware.js
+++ middleware.js
@@ -11,11 +11,13 @@
 //page every time so we get error: localhost redirected you too many times.
 // that's why we need to define for which route to run the middleware
 //matcher
-
+// When you import auth from ./app/_lib/auth (assuming it’s set up with NextAuth/Auth.js), it provides a pre-built, fully functional
+// middleware solution for authentication. It handles a lot of heavy lifting behind the scenes.
 import { auth } from "./app/_lib/auth";
 //the auth function that's exported from nextauth can also be used as middleware.
 //we renamed here auth as middleware
 export const middleware = auth;
+//here we imported auth and assinged it as a middleware
 
 export const config = {
   matcher: ["/account"], //now the middleware only runs for account route, can add as many routes as we want
